187c33ab5932ce794d8c35147b4e8b3137c9db33
justforlxz <justforlxz@gmail.com>
2023-01-03 17:56:57 +0800

    enable all refactoring

--- lua/plugins/refactoring.lua
+++ lua/plugins/refactoring.lua
@@ -14,21 +14,6 @@ return {
     },
   },
   config = function()
-    require("refactoring").setup({
-      -- prompt for return type
-      prompt_func_return_type = {
-        go = true,
-        cpp = true,
-        c = true,
-        java = true,
-      },
-      -- prompt for function parameters
-      prompt_func_param_type = {
-        go = true,
-        cpp = true,
-        c = true,
-        java = true,
-      },
-    })
+    require("refactoring").setup({})
   end,
 }
