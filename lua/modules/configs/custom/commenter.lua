e01f624cea77808018f473ef7b9bea6b761d8b4b
justforlxz <justforlxz@gmail.com>
2023-02-09 16:56:03 +0800

    add markdown-preview

---
+++ lua/modules/configs/custom/commenter.lua
@@ -0,0 +1,4 @@
+return function()
+  vim.g.NERDSpaceDelims = 1
+  vim.g.NERDCreateDefaultMappings = 0
+end
--- lua/modules/plugins/custom.lua
+++ lua/modules/plugins/custom.lua
@@ -1,2 +1,20 @@
 local custom = {}
+
+custom["iamcco/markdown-preview.nvim"] = {
+	build = "cd app && yarn install",
+	ft = "md",
+}
+
+custom["lambdalisue/suda.vim"] = {
+	cmd = {
+		"SudaRead",
+		"SudaWrite",
+	},
+}
+
+custom["preservim/nerdcommenter"] = {
+	config = require("custom.commenter"),
+	event = { "BufReadPost", "BufAdd", "BufNewFile" },
+}
+
 return custom
