10741c9e1d7b7916ae0437454782300b2980974e
justforlxz <justforlxz@gmail.com>
2023-03-31 09:14:11 +0800

    use anuvyklack/windows.nvim

--- lua/modules/plugins/editor.lua
+++ lua/modules/plugins/editor.lua
@@ -100,4 +100,12 @@ editor["nvim-treesitter/nvim-treesitter"] = {
 	},
 }
 
+editor["anuvyklack/windows.nvim"] = {
+   dependencies = {
+      "anuvyklack/middleclass",
+      "anuvyklack/animation.nvim"
+   },
+   config = require("editor.windows"),
+}
+
 return editor
