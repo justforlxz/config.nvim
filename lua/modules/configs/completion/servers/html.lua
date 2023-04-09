fa9795a522f0bbe929e00293a35d05b447e91064
groveer <groveer@qq.com>
2023-04-09 12:32:33 +0800

    fix(html): cmd line error

--- lua/modules/configs/completion/servers/html.lua
+++ lua/modules/configs/completion/servers/html.lua
@@ -1,12 +1,11 @@
 -- https://github.com/vscode-langservers/vscode-html-languageserver-bin
 return {
-	cmd = { "html-languageserver", "--stdio" },
-	filetypes = { "html" },
-	init_options = {
-		configurationSection = { "html", "css", "javascript" },
-		embeddedLanguages = { css = true, javascript = true },
-	},
-	settings = {},
-	single_file_support = true,
-	flags = { debounce_text_changes = 500 },
+    filetypes = { "html" },
+    init_options = {
+        configurationSection = { "html", "css", "javascript" },
+        embeddedLanguages = { css = true, javascript = true },
+    },
+    settings = {},
+    single_file_support = true,
+    flags = { debounce_text_changes = 500 },
 }
