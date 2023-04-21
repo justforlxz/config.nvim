d33992a4e44bf73135bd6e706f5376f865390335
groveer <groveer@outlook.com>
2023-04-21 16:39:32 +0800

    fix: support bufferline for v3.7.0

--- lua/modules/configs/ui/bufferline.lua
+++ lua/modules/configs/ui/bufferline.lua
@@ -1,49 +1,76 @@
 return function()
-	local icons = { ui = require("modules.utils.icons").get("ui") }
+    local icons = {
+        ui = require("modules.utils.icons").get("ui"),
+        diagnostics = require("modules.utils.icons").get("diagnostics")
+    }
 
-	local opts = {
-		options = {
-			number = nil,
-			modified_icon = icons.ui.Modified,
-			buffer_close_icon = icons.ui.Close,
-			left_trunc_marker = icons.ui.Left,
-			right_trunc_marker = icons.ui.Right,
-			max_name_length = 14,
-			max_prefix_length = 13,
-			tab_size = 20,
-			color_icons = true,
-			show_buffer_icons = true,
-			show_buffer_close_icons = true,
-			show_buffer_default_icon = true,
-			show_close_icon = true,
-			show_tab_indicators = true,
-			enforce_regular_tabs = true,
-			persist_buffer_sort = true,
-			always_show_bufferline = true,
-			separator_style = "thin",
-			diagnostics = "nvim_lsp",
-			diagnostics_indicator = function(count)
-				return "(" .. count .. ")"
-			end,
-			offsets = {
-				{
-					filetype = "NvimTree",
-					text = "File Explorer",
-					text_align = "center",
-					padding = 1,
-				},
-				{
-					filetype = "lspsagaoutline",
-					text = "Lspsaga Outline",
-					text_align = "center",
-					padding = 1,
-				},
-			},
-		},
-		-- Change bufferline's highlights here! See `:h bufferline-highlights` for detailed explanation.
-		-- Note: If you use catppuccin then modify the colors below!
-		highlights = {},
-	}
+    local opts = {
+        options = {
+            mode = "buffers",                    -- set to "tabs" to only show tabpages instead
+            themable = true,                     -- allows highlight groups to be overriden i.e. sets highlights as default
+            numbers = "none",
+            close_command = "bdelete! %d",       -- can be a string | function, | false see "Mouse actions"
+            right_mouse_command = "bdelete! %d", -- can be a string | function | false, see "Mouse actions"
+            left_mouse_command = "buffer %d",    -- can be a string | function, | false see "Mouse actions"
+            middle_mouse_command = nil,          -- can be a string | function, | false see "Mouse actions"
+            indicator = {
+                icon = '▎',                    -- this should be omitted if indicator style is not 'icon'
+                style = 'none',
+            },
+            modified_icon = icons.ui.Modified,
+            buffer_close_icon = icons.ui.Close,
+            left_trunc_marker = icons.ui.Left,
+            right_trunc_marker = icons.ui.Right,
+            max_name_length = 16,
+            max_prefix_length = 13, -- prefix used when a buffer is de-duplicated
+            truncate_names = true,  -- whether or not tab names should be truncated
+            diagnostics = "nvim_lsp",
+            diagnostics_update_in_insert = false,
+            -- The diagnostics indicator can be set to nil to keep the buffer name highlight but delete the highlighting
+            diagnostics_indicator = function(count, level, diagnostics_dict, context)
+                local s = ""
+                for e, n in pairs(diagnostics_dict) do
+                    local sym = e == "error" and icons.diagnostics.Error_alt
+                        or (e == "warning" and icons.diagnostics.Warning_alt or icons.diagnostics.Information_alt)
+                    s = s .. n .. sym .. " "
+                end
+                return s
+            end,
+            offsets = {
+                {
+                    filetype = "NvimTree",
+                    text = "File Explorer",
+                    text_align = "left",
+                    separator = true
+                }
+            },
+            color_icons = true, -- whether or not to add the filetype icon highlights
+            get_element_icon = function(element)
+                -- element consists of {filetype: string, path: string, extension: string, directory: string}
+                -- This can be used to change how bufferline fetches the icon
+                -- for an element e.g. a buffer or a tab.
+                -- e.g.
+                local icon, hl = require('nvim-web-devicons').get_icon_by_filetype(element.filetype, { default = false })
+                return icon, hl
+            end,
+            show_buffer_icons = true, -- disable filetype icons for buffers
+            show_buffer_close_icons = false,
+            show_close_icon = false,
+            show_tab_indicators = false,
+            show_duplicate_prefix = true, -- whether to show duplicate buffer prefix
+            persist_buffer_sort = true,   -- whether or not custom sorted buffers should persist
+            -- can also be a table containing 2 custom separators
+            -- [focused and unfocused]. eg: { '|', '|' }
+            separator_style = { '|', '|' },
+            enforce_regular_tabs = false,
+            always_show_bufferline = true,
+            hover = {
+                enabled = true,
+                delay = 200,
+                reveal = { 'close' }
+            },
+        },
+    }
 
 	if vim.g.colors_name == "catppuccin" then
 		local cp = require("modules.utils").get_palette() -- Get the palette.
