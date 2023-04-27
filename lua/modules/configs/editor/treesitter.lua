5bbe7ba84e6b271db8d482e3d6b4d1d31051d8a7
justforlxz <justforlxz@gmail.com>
2023-04-27 14:56:47 +0800

    fix ts-autotag not working

--- lua/modules/configs/editor/treesitter.lua
+++ lua/modules/configs/editor/treesitter.lua
@@ -56,6 +56,9 @@ return vim.schedule_wrap(function()
 		},
 		context_commentstring = { enable = true, enable_autocmd = false },
 		matchup = { enable = true },
+		autotag = {
+			enable = true,
+		}
 	})
 	require("nvim-treesitter.install").prefer_git = true
 	if use_ssh then
