0db9920445768a17ae311f0cc42801272a6908e8
justforlxz <justforlxz@gmail.com>
2023-04-27 10:57:22 +0800

    format lua

--- lua/modules/configs/completion/null-ls.lua
+++ lua/modules/configs/completion/null-ls.lua
@@ -1,44 +1,44 @@
 return function()
-	local null_ls = require("null-ls")
-	local mason_null_ls = require("mason-null-ls")
-	local btns = null_ls.builtins
+    local null_ls = require("null-ls")
+    local mason_null_ls = require("mason-null-ls")
+    local btns = null_ls.builtins
 
-	-- Please set additional flags for the supported servers here
-	-- Don't specify any config here if you are using the default one.
-	local sources = {
-		btns.formatting.black.with({
-			extra_args = { "--fast" },
-		}),
-		btns.formatting.prettier.with({
-			filetypes = {
-				"vue",
-				"typescript",
-				"javascript",
-				"typescriptreact",
-				"javascriptreact",
-				"yaml",
-				"html",
-				"css",
-				"scss",
-				"sh",
-				"markdown",
-			},
-		}),
-	}
-	null_ls.setup({
-		border = "rounded",
-		debug = false,
-		log_level = "warn",
-		update_in_insert = false,
-		sources = sources,
-	})
+    -- Please set additional flags for the supported servers here
+    -- Don't specify any config here if you are using the default one.
+    local sources = {
+        btns.formatting.black.with({
+            extra_args = { "--fast" },
+        }),
+        btns.formatting.prettier.with({
+            filetypes = {
+                "vue",
+                "typescript",
+                "javascript",
+                "typescriptreact",
+                "javascriptreact",
+                "yaml",
+                "html",
+                "css",
+                "scss",
+                "sh",
+                "markdown",
+            },
+        }),
+    }
+    null_ls.setup({
+        border = "rounded",
+        debug = false,
+        log_level = "warn",
+        update_in_insert = false,
+        sources = sources,
+    })
 
-	mason_null_ls.setup({
-		ensure_installed = require("core.settings").null_ls_deps,
-		automatic_installation = false,
-		automatic_setup = true,
+    mason_null_ls.setup({
+        ensure_installed = require("core.settings").null_ls_deps,
+        automatic_installation = false,
+        automatic_setup = true,
         handlers = {},
-	})
+    })
 
-	require("completion.formatting").configure_format_on_save()
+    require("completion.formatting").configure_format_on_save()
 end
