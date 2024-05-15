4f19b6749a3c22e0239d22f3192a2a2874fbeeca
justforlxz <justforlxz@gmail.com>
2024-05-15 13:01:15 +0800

    update plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,8 +7,8 @@ local map = Util.safe_keymap_set
 
 map({ "n", "v" }, "p", 'p:let @+=@0<CR>:let @"=@0<CR>', { desc = "Dont copy replaced text" })
 
-map("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Next buffer" })
-map("n", "<S-Tab>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+-- map("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+-- map("n", "<S-Tab>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
 map("n", "<A-f>", function()
   require("conform").format()
   vim.notify("Format successful!")
@@ -21,3 +21,21 @@ map("t", "<A-d>", "<Cmd>ToggleTerm<CR>", { desc = "terminal: Toggle float" })
 -- Plugin: lspsaga
 map("n", "<leader>ca", "<Cmd>Lspsaga code_action<CR>", { desc = "Lsp: code action" })
 map("n", "gh", "<Cmd>Lspsaga finder<CR>", { desc = "Lsp: peek definition" })
+
+-- Plugin: smart-split
+-- recommended mappings
+vim.keymap.set('n', '<A-h>', require('smart-splits').resize_left)
+vim.keymap.set('n', '<A-j>', require('smart-splits').resize_down)
+vim.keymap.set('n', '<A-k>', require('smart-splits').resize_up)
+vim.keymap.set('n', '<A-l>', require('smart-splits').resize_right)
+-- moving between splits
+vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
+vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
+vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
+vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
+vim.keymap.set('n', '<C-\\>', require('smart-splits').move_cursor_previous)
+-- swapping buffers between windows
+vim.keymap.set('n', '<leader><leader>h', require('smart-splits').swap_buf_left)
+vim.keymap.set('n', '<leader><leader>j', require('smart-splits').swap_buf_down)
+vim.keymap.set('n', '<leader><leader>k', require('smart-splits').swap_buf_up)
+vim.keymap.set('n', '<leader><leader>l', require('smart-splits').swap_buf_right)
---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,10 @@
+return {
+  "hrsh7th/nvim-cmp",
+  opts = {
+    view = {
+      entries = {
+        follow_cursor = true,
+      },
+    },
+  },
+}
