3a8c7aa99d0e5b6e21a18942eb46f5d69ba9a899
justforlxz <justforlxz@gmail.com>
2025-09-10 11:33:19 +0800

    update config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,6 +17,11 @@ require("lazy").setup({
     { import = "lxz.modules.ai.codecompanion" },
 
     -- import/override with your plugins
+    { import = "lxz.plugins.ai.codecompanion.codecompanion-cmp" },
+    { import = "lxz.plugins.ai.codecompanion.codecompanion" },
+    { import = "lxz.plugins.ai.codecompanion.copilot-cmp" },
+    { import = "lxz.plugins.ai.codecompanion.copilot" },
+    { import = "lxz.plugins.ai.codecompanion.mcphub" },
     { import = "lxz.plugins.coding.yanky" },
     { import = "lxz.plugins.coding.crates" },
     { import = "lxz.plugins.editor.blink" },
--- lua/lxz/modules/ai/codecompanion.lua
+++ lua/lxz/modules/ai/codecompanion.lua
@@ -4,11 +4,4 @@ return {
   { "groveer/nvvim", import = "nvvim.plugins.ai.copilot-cmp" },
   { "groveer/nvvim", import = "nvvim.plugins.ai.copilot" },
   { "groveer/nvvim", import = "nvvim.plugins.ai.mcphub" },
-
-  --- override
-  { import = "lxz.plugins.ai.codecompanion.codecompanion-cmp" },
-  { import = "lxz.plugins.ai.codecompanion.codecompanion" },
-  { import = "lxz.plugins.ai.codecompanion.copilot-cmp" },
-  { import = "lxz.plugins.ai.codecompanion.copilot" },
-  { import = "lxz.plugins.ai.codecompanion.mcphub" },
 }
